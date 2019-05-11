e9f7e9feed2e410342affa9c0b2d75858b833915
srinivasthadi <tsri0814@gmail.com>
2019-05-11 12:20:53 +0530

    1 file added

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatButtonToggleModule, MatIconModule } from '@angular/material';
+
+const MaterialComponents = [ MatButtonModule, MatButtonToggleModule, MatIconModule  ];
+
+@NgModule({
+ imports: [MaterialComponents],
+ exports: [MaterialComponents]
+})
+export class MaterialModule { }
